edd1a2764f87d6953da74374c3e7b21803d5323f
Nathan Manceaux-Panot <cykelero@gmail.com>
2013-05-29 23:03:15 +0200

    SandboxHistory: Added basic example

--- workingSource/sandboxHistory.js
+++ workingSource/sandboxHistory.js
@@ -1,3 +1,13 @@
 var generated = FunnelInstanceSignaturePattern("firstname: string, surname: string, age: number");
 
 console.log(generated(["Nathan", "Manceaux-Panot", 20]));
+
+
+
+echoName = Funnel
+	("firstname: string, surname: string")
+(function(firstname, surname) {
+	console.log(firstname + ", " + surname);
+});
+
+echoName("John", "Doe");
